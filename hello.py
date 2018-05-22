9660f4598e4e53b379b04dc430b37ab0f67f5aef
Liu Zifeng <liuzifeng@doodod.com>
2018-05-22 14:06:58 +0800

    perf:添加异常处理

--- hello.py
+++ hello.py
@@ -11,7 +11,7 @@ def hello():
     return 'hello'
 
 # 用于第一次登陆，如果此人是第一次登陆，则建库储存个人基本信息(ok为0表示此用户第一次登陆，为1表示老用户）
-#  Done
+#  Done（无需异常处理）
 @app.route('/user/<user_id>/name/<name>')
 def setup_user(user_id,name):
     the_user = User.query.filter_by(user_id=user_id).first()
@@ -35,7 +35,7 @@ def setup_user(user_id,name):
     return results_json
 
 # 用于返回具体某个词 根据唯一标识这个词对应的id 同时储存用户id浏览记录
-#  Done
+#  Done（无需异常处理）
 @app.route('/user/<user_id>/poetry/<id>')
 def get_poetry(user_id,id):
     results = Ci.query.filter_by(sn=id).first()
@@ -63,7 +63,7 @@ def get_poetry(user_id,id):
     return results_json
 
 # 根据诗人返回符合的词
-#  Done
+#  Done（已异常处理）
 @app.route('/author/<keyword>')
 def poetry_find_by_author(keyword):
     results = Ci.query.filter(Ci.author.like("%" + keyword + "%")).all()
@@ -82,7 +82,7 @@ def poetry_find_by_author(keyword):
     return results_json
 
 # 根据内容返回符合的词（！！！请搜索短句！！！可以优化，但需要时间，暂且不管）
-#  Done
+#  Done（已异常处理）
 @app.route('/paragraphs/<keyword>')
 def poetry_find_by_paragraphs(keyword):
     results = Ci.query.filter(Ci.paragraphs.like("%" + keyword + "%")).all()
@@ -101,7 +101,7 @@ def poetry_find_by_paragraphs(keyword):
     return results_json
 
 # 根据词牌返回符合的词
-#  Done
+#  Done（已异常处理）
 @app.route('/rhythmic/<keyword>')
 def poetry_find_by_rhythmic(keyword):
     results = Ci.query.filter(Ci.rhythmic.like("%" + keyword + "%")).all()
@@ -120,7 +120,7 @@ def poetry_find_by_rhythmic(keyword):
     return results_json
 
 # 根据用户名，词牌名，词的内容，进行储存(有字数限制 512个字，包含标点），赋予“我的创作”编号
-#  Done
+#  Done（无需异常处理）
 @app.route('/user/<user_id>/user_name/<name>/poetry/<words>/rhythmic/<rhythmic>')
 def save_poetry(user_id,name,words,rhythmic):
     config=configparser.ConfigParser()
@@ -143,7 +143,7 @@ def save_poetry(user_id,name,words,rhythmic):
     return "{\"ok\":1}"
 
 # 储存评论，需要发表人的id以及被评论的id
-#  Done
+#  Done（无需异常处理）
 @app.route('/user_name/<name>/finished/<sn>/comment/<comment>')
 def save_comments(name,sn,comment):
     finished = Finished.query.filter_by(sn=sn).first()
@@ -157,7 +157,7 @@ def save_comments(name,sn,comment):
     return "{\"ok\":1}"
 
 # 根据用户名，词牌名，词的内容，进行储存(默认会储存新的创作）
-#  Done
+#  Done（无需异常处理）
 @app.route('/user/<user_id>/user_name/<name>/iscreating/<words>/rhythmic/<rhythmic>')
 def save_iscreating(user_id,name,words,rhythmic):
     config = configparser.ConfigParser()
@@ -180,7 +180,7 @@ def save_iscreating(user_id,name,words,rhythmic):
     return "{\"ok\":1}"
 
 # 根据完成作品编号返回对应作品
-#  Done
+#  Done（无需异常处理）
 @app.route('/finished/<sn>')
 def poetry_find_by_finished_sn(sn):
     result = Finished.query.filter_by(sn=sn).first()
@@ -199,7 +199,7 @@ def poetry_find_by_finished_sn(sn):
     return results_json
 
 # 根据正在创作作品编号返回对应作品
-#  Done
+#  Done（无需异常处理）
 @app.route('/iscreating/<sn>')
 def poetry_find_by_iscreating(sn):
     result = IsCreating.query.filter_by(sn=sn).first()
@@ -213,66 +213,73 @@ def poetry_find_by_iscreating(sn):
     return results_json
 
 # 根据word 返回这个字对应的平仄
-#  Done
+#  Done（已异常处理）
 @app.route('/pingze/<words>')
 def get_pingze(words):
-    test = PinYin()
-    test.load_word()
-    results = test.hanzi2yindiao(string=words)
-    pz = []
-    for result in results:
-        x1, x2, x3 = 0, 0, 0
-        for i in result:
-            if i == '1' or i == '2':
-                x1 = 1
+    try:
+        test = PinYin()
+        test.load_word()
+        results = test.hanzi2yindiao(string=words)
+        pz = []
+        for result in results:
+            x1, x2, x3 = 0, 0, 0
+            for i in result:
+                if i == '1' or i == '2':
+                    x1 = 1
+                    continue
+                if i == '3' or i == '4':
+                    x2 = 1
+                    continue
+                if i == '5':
+                    x1 = 1
+                    x2 = 1
+                    continue
+                x3 = 1
+            if x1 == 1 and x2 == 1:
+                pz.append('中')
                 continue
-            if i == '3' or i == '4':
-                x2 = 1
+            if x1 == 1 and x2 == 0:
+                pz.append('平')
                 continue
-            if i == '5':
-                x1 = 1
-                x2 = 1
+            if x1 == 0 and x2 == 1:
+                pz.append('仄')
                 continue
-            x3 = 1
-        if x1 == 1 and x2 == 1:
-            pz.append('中')
-            continue
-        if x1 == 1 and x2 == 0:
-            pz.append('平')
-            continue
-        if x1 == 0 and x2 == 1:
-            pz.append('仄')
-            continue
-        if x3 == 1:
-            pz.append('空')
-    return str(pz)
+            if x3 == 1:
+                pz.append('空')
+        result_json = "{\"ok\":1,\"result\":\"" + str(pz) + "\"}"
+    except:
+        result_json = "{\"ok\":0,\"}"
+    return result_json
 
 # 根据words 返回这些字是否韵脚相同 并返回对应的韵脚推荐(返回{"ok":0}表示韵脚不统一，或者多音字不确定韵脚，若统一则返回推荐韵脚）
-#  Done
+#  Done（已异常处理）
 @app.route('/yunjiao/<words>')
 def get_yunjiao(words):
-    test = PinYin()
-    test.load_word()
-    results = test.hanzi2yunjiao(string=words)
-    for index, item in enumerate(results):
-        if index == 0:
-            first_yj = item
+    try:
+        test = PinYin()
+        test.load_word()
+        results = test.hanzi2yunjiao(string=words)
+        for index, item in enumerate(results):
+            if index == 0:
+                first_yj = item
+            else:
+                ok = 0
+                for i in item:
+                    if i in first_yj:
+                        first_yj = [i]
+                        ok = 1
+                if ok == 0:
+                    return "{\"ok\":0,\"suggestion\":\"[]\"}"
+        if len(first_yj) == 1:
+            suggestion = test.get_yunjiao(first_yj[0])
+            return "{\"ok\":1,\"suggestion\":\"" + str(suggestion) + "\"}"
         else:
-            ok = 0
-            for i in item:
-                if i in first_yj:
-                    first_yj = [i]
-                    ok = 1
-            if ok == 0:
-                return "{\"ok\":0,\"suggestion\":\"[]\"}"
-    if len(first_yj) == 1:
-        suggestion = test.get_yunjiao(first_yj[0])
-        return "{\"ok\":1,\"suggestion\":\"" + str(suggestion) + "\"}"
-    else:
-        return "{\"ok\":0,\"suggestion\":\"" + str(first_yj) + "\"}"
+            return "{\"ok\":0,\"suggestion\":\"" + str(first_yj) + "\"}"
+    except:
+        return "{\"ok\":0}"
 
 # 根据词牌名返回词牌对应格式
-#  Done
+#  Done（已异常处理）
 @app.route('/rhythmic_rule/<keyword>')
 def get_rhythmic_rule(keyword):
     keys = [x for x in pingzes.items() if keyword in x[0]]
